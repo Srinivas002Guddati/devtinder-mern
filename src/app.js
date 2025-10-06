611460282ef12f79c64c59fa9015a14816bda3d4
Srinivas G <srinivasg.mca002@gmail.com>
2025-10-06 14:00:59 +0530

    password encryption

--- src/app.js
+++ src/app.js
@@ -2,23 +2,36 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./model/user');
+const { validateSignupData } = require('./utils/validation');
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res)=>{
-    console.log("req", req.body);
-    
+  
     try{
+        const { firstName, lastName, emailId, password } = req.body;
+        //validation of data
+        validateSignupData(req);
+
+        //Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+
         const existingUser = await User.findOne({emailId: req.body.emailId});
         if(existingUser){
             return res.status(409).send("Email already existing");
         }
-        const user = new User(req.body);
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
         await user.save();    
         res.send("User added successfully");
     }catch (err){
-        res.status(400).send("Error saving the user: ", err.message); 
+        res.status(400).send("Error saving the user: " + err.message); 
     }
 });
 
@@ -53,16 +66,30 @@ app.delete("/user", async (req, res)=>{
     }
 });
 
-app.patch("/user", async (req, res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
 
     try{
+        const ALLOWED_UPDATES = [
+            "photoUrl",
+            "about",
+            "gender",
+            "age",
+            "skills"
+        ];
+        const isUpdateAllowed = Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k))
+        if(!isUpdateAllowed){
+            throw new Error("Udate not allowed");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
         const existingUser = await User.findOne({emailId: req.body.emailId});
         if(existingUser){
             return res.status(409).send("Email already existing");
         }
-
         const user = await User.findByIdAndUpdate({_id: userId}, data,{
             returnDocument: "after",
             runValidators: true
@@ -70,7 +97,7 @@ app.patch("/user", async (req, res)=>{
         console.log("");
         res.send("User updated successfully");
     }catch (err){
-        res.status(404).send("Something went wrong");
+        res.status(400).send(err.message);
     }
 });
 
@@ -81,7 +108,7 @@ connectDB().then(()=>{
 });
 
 }).catch((err)=>{
-    console.log("Database connection established...");
+    console.log("Connection issues...");
 });
 
 
--- src/model/user.js
+++ src/model/user.js
@@ -1,34 +1,46 @@
 
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-        type:String,
-        required: true,
-        minLength: 4
-    },
-    lastName: {
-        type: String
-    },
-    emailId: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    age: {
+firstName: {
+    type:String,
+    required: true,
+    minLength: 4
+},
+lastName: {
+    type: String
+},
+emailId: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+        validator: function (value) {
+        return validator.isEmail(value);
+        },
+        message: props => `Invalid email address: ${props.value}`
+    }
+},
+password: {
+    type: String,
+    validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a strong password");
+        }
+    }
+
+},
+ age: {
         type: Number
     },
-    gener: {
-        type: String,
-        validate(value){
-            if(!['male', 'female', 'others'].includes(value));
-            throw new Error("Gender data is not valid")
-        }
+    gender: {
+        type: String
     },
     photoUrl:{
-        type: String
+        type: String,
     },
     about: {
         type: String,
@@ -36,7 +48,7 @@ const userSchema = new mongoose.Schema({
     },
     skills: {
         type: [String]
-    }      
+    }    
     
 },{
     timestamps: true
